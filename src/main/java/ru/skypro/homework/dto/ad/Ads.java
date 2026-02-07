371337814691f3eb5b8b66376a121844188fb77a
yagitpa <yakov.palchikov@gmail.com>
2026-02-08 03:59:57 +0700

    Issue: #6 DIP-4
    1. Валидация полей:
    - все обязательные поля отмечены @NotBlank
    - для CreateOrUpdateAd: title, price, description
    2. Аннотации:
    - каждый класс имеет @Schema с описанием
    - каждое поле содержит примеры, ограничения и описания
    3. Разделение ответственности:
    - CreateOrUpdateAd - для создания/обновления
    - Ad - для краткого представления
    - ExtendedAd - для детального представления

---
+++ src/main/java/ru/skypro/homework/dto/ad/Ads.java
@@ -0,0 +1,22 @@
+package ru.skypro.homework.dto.ad;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Schema(description = "Обертка для списка объявлений")
+public class Ads {
+
+    @Schema(
+            description = "Общее количество объявлений",
+            example = "10"
+    )
+    private Integer count;
+
+    @Schema(
+            description = "Список объявлений"
+    )
+    private List<Ad> results;
+}
