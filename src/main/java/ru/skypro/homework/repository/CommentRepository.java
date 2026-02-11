18a0adb2d72f97738079d6e0566626d35cdaff95
yagitpa <yakov.palchikov@gmail.com>
2026-02-11 22:53:47 +0700

    Issue #31: DIP-19 Реализация CommentRepository с сортировкой
    1. Добавлен репозиторий сущности `Comment`:
    - методы без пагинации
    - методы с поддержкой пагинации

---
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -0,0 +1,62 @@
+package ru.skypro.homework.repository;
+
+import ru.skypro.homework.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    // ---------- Методы без пагинации ----------
+
+    /**
+     * Получение всех комментариев к объявлению, отсортированных по дате создания (сначала новые).
+     * Без пагинации – возвращает полный список, например, для экспорта.
+     *
+     * @param adPk идентификатор объявления (ad.pk)
+     * @return список комментариев
+     */
+    List<Comment> findByAdPkOrderByCreatedAtDesc(Integer adPk);
+
+    /**
+     * Поиск комментария по его идентификатору и идентификатору объявления.
+     *
+     * @param id   идентификатор комментария (comment.pk)
+     * @param adPk идентификатор объявления (ad.pk)
+     * @return Optional с комментарием или пустой Optional
+     */
+    Optional<Comment> findByIdAndAdPk(Integer id, Integer adPk);
+
+    /**
+     * Поиск комментария по его идентификатору и идентификатору автора.
+     *
+     * @param id       идентификатор комментария (comment.pk)
+     * @param authorId идентификатор автора (author.id)
+     * @return Optional с комментарием или пустой Optional
+     */
+    Optional<Comment> findByIdAndAuthorId(Integer id, Integer authorId);
+
+    // ---------- Методы с пагинацией ----------
+
+    /**
+     * Получение комментариев к объявлению с пагинацией и сортировкой по дате создания (новые первыми).
+     *
+     * @param adPk     идентификатор объявления (ad.pk)
+     * @param pageable параметры пагинации и сортировки
+     * @return страница комментариев
+     */
+    Page<Comment> findByAdPk(Integer adPk, Pageable pageable);
+
+    /**
+     * Подсчёт количества комментариев у объявления.
+     *
+     * @param adPk идентификатор объявления
+     * @return количество комментариев
+     */
+    long countByAdPk(Integer adPk);
+}
