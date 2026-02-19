ccfc20e4fe961f05b2d6a621c5600e41e8fe0ddd
yagitpa <yakov.palchikov@gmail.com>
2026-02-20 03:00:03 +0700

    Проблема локализована, все тесты проходят кроме одного. Исправляю.

--- src/test/java/ru/skypro/homework/HomeworkApplicationTests.java
+++ src/test/java/ru/skypro/homework/HomeworkApplicationTests.java
@@ -3,8 +3,8 @@ package ru.skypro.homework;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HomeworkApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HomeworkApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {}
