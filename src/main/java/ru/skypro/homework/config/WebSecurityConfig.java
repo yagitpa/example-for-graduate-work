f5747fa769484e75c7d674c175f73a3c007b1648
yagitpa <yakov.palchikov@gmail.com>
2026-02-28 01:27:08 +0700

    Небольшие косметические правки

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
+++ src/main/java/ru/skypro/homework/filter/BasicAuthCorsFilter.java
@@ -1,25 +1,25 @@
-package ru.skypro.homework.filter;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@Component
-public class BasicAuthCorsFilter extends OncePerRequestFilter {
-
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest httpServletRequest,
-            HttpServletResponse httpServletResponse,
-            FilterChain filterChain)
-            throws ServletException, IOException {
-        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
-    }
-}
+//package ru.skypro.homework.filter;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@Component
+//public class BasicAuthCorsFilter extends OncePerRequestFilter {
+//
+//    @Override
+//    protected void doFilterInternal(
+//            HttpServletRequest httpServletRequest,
+//            HttpServletResponse httpServletResponse,
+//            FilterChain filterChain)
+//            throws ServletException, IOException {
+//        httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
+//        filterChain.doFilter(httpServletRequest, httpServletResponse);
+//    }
+//}
